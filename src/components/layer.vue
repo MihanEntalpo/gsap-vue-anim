8aca7d1cfd823341ba90768b79dfa2d726eb634b
MihanEntalpo <mihanentalpo@yandex.ru>
2019-06-13 00:21:46 +0700

    Added layers

---
+++ src/components/layer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="anim-container layer" :style="style"><slot></slot></div>
+</template>
+
+<script> 
+export default { 
+  mounted() { 
+    
+  },
+  props: {
+     "opacity": {
+         "default": 1
+     }
+  },
+  computed: {
+      "style": function(){ return "opacity:" + this.opacity; }
+  }
+} 
+</script>
+
+<style scoped>
+    .anim-container.layer
+    {
+        position:absolute;
+        left:0px;
+        top:0px;
+        width:100vw;
+        height:100vh;
+    }
+</style>
--- src/components/scene_1.vue
+++ src/components/scene_1.vue
@@ -2,22 +2,25 @@
     <div class="animation">
         <background>
             <div class="anim-container">
-                <mover acid="ball-mover">
-                    <ball acid="ball"/>
-                </mover>
-                <mover acid="capital-move" x="200" y="500">
-                    <spin speed="0.02" acid="capital-spin">
-                        <heading  size="1" acid="capital-heading">Ах у ели, ах у ёлки, ах у ели злые волки</heading>
-                    </spin>
-                </mover>
-                <mover :x="test.x1" :y="test.y1" acid="test-mover"><heading size=1 acid="test-heading">test</heading></mover>
-                <heading  size="2" >Еду я по выбоине, из выбоины не выеду я</heading>
-                <heading  size="3" >Улыбок тебе дед мокар!</heading>
-                <heading  size="4" >Ох, у ямы холм с кулями, Выйду на холм - куль поправлю.</heading>
-                <div class="box-container" ref=box1 >
-                    <box id="redbox-1" size="200" color="blue" speed="3"/>
-                </div>
-
+                <layer acid="ball-layer">
+                    <mover acid="ball-mover" x="100" y="100">
+                        <ball acid="ball"/>
+                    </mover>
+                </layer>
+                <layer acid="experiments-layer" opacity="0">
+                    <mover acid="capital-move" x="200" y="500">
+                        <spin speed="0.02" acid="capital-spin">
+                            <heading  size="1" acid="capital-heading">Ах у ели, ах у ёлки, ах у ели злые волки</heading>
+                        </spin>
+                    </mover>
+                    <mover :x="test.x1" :y="test.y1" acid="test-mover"><heading size=1 acid="test-heading">test</heading></mover>
+                    <heading  size="2" >Еду я по выбоине, из выбоины не выеду я</heading>
+                    <heading  size="3" >Улыбок тебе дед мокар!</heading>
+                    <heading  size="4" >Ох, у ямы холм с кулями, Выйду на холм - куль поправлю.</heading>
+                    <div class="box-container" ref=box1 >
+                        <box id="redbox-1" size="200" color="blue" speed="3"/>
+                    </div>
+                </layer>
             </div>
         </background>
     </div>
@@ -31,6 +34,7 @@ import heading from './heading.vue';
 import spin from './spin.vue';
 import ball from './ball.vue';
 import mover from './mover.vue';
+import layer from './layer.vue';
 import { Linear, TweenMax } from 'gsap'
     
 export default {
@@ -50,28 +54,34 @@ export default {
      
   },
   components: {
-      box, background, heading, spin, ball, mover
+      box, background, heading, spin, ball, mover, layer
   },
   mounted() {
-        const box1 = this.$refs.box1;
+        
         //const timeline = new TimelineLite();
         var r = 150;
         var xc = parseInt(window.innerWidth / 2);
         var yc = parseInt(window.innerHeight / 2);
         
         
-        TweenMax.to(this.test, 5, {x1:500, y1:500, repeat: -1})
+        const box1 = this.$refs.box1;
+        if (box1)
+        {
+            TweenMax.set(box1, {x: xc , y: yc - r});
+            TweenMax.to(box1, 5, {bezier:{type:'thru', values:
+                            [
+                                /*p1*/{x:xc, y:yc - r},
+                                /*p2*/{x:xc + r, y:yc},
+                                /*p3*/{x:xc, y:yc + r},
+                                /*p4*/{x:xc - r, y:yc},
+                                      {x:xc, y:yc - r}
+                            ]
+                    }, ease:Linear.easeNone, repeat: -1 });
+        }
         
-        TweenMax.set(box1, {x: xc , y: yc - r});
-        TweenMax.to(box1, 5, {bezier:{type:'thru', values:
-                        [
-                            /*p1*/{x:xc, y:yc - r},
-                            /*p2*/{x:xc + r, y:yc},
-                            /*p3*/{x:xc, y:yc + r},
-                            /*p4*/{x:xc - r, y:yc},
-                                  {x:xc, y:yc - r}
-                        ]
-                }, ease:Linear.easeNone, repeat: -1 });
+        TweenMax.to(this.test, 5, {x1:500, y1:500})
+        
+       
         
   } 
 }
